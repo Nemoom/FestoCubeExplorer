d448a4aaab46ae8aed014c697177ca09a94332c6
Tobby Li <tobby.li@festo.com>
2020-03-12 15:13:17 +0800

    Generate string

--- Festo Rubik‘s Cube Explorer/Form1.cs
+++ Festo Rubik‘s Cube Explorer/Form1.cs
@@ -86,5 +86,74 @@ namespace Festo_Rubik_s_Cube_Explorer
                 ((Button)sender).BackColor = Color.White;
             }
         }
+
+        private string GetColor(Button mButton)
+        {
+            string mColor = " ";
+            if (mButton.BackColor == btn_U5.BackColor) { mColor = "U"; }
+            else if (mButton.BackColor == btn_R5.BackColor) { mColor = "R"; }
+            else if (mButton.BackColor == btn_F5.BackColor) { mColor = "F"; }
+            else if (mButton.BackColor == btn_D5.BackColor) { mColor = "D"; }
+            else if (mButton.BackColor == btn_L5.BackColor) { mColor = "L"; }
+            else if (mButton.BackColor == btn_B5.BackColor) { mColor = "B"; }
+            return mColor;
+        }
+        private void tableLayoutPanel_Cube_DoubleClick(object sender, EventArgs e)
+        {
+            textBox1.Text = GetColor(btn_U1) 
+                        + GetColor(btn_U2)
+                        + GetColor(btn_U3)
+                        + GetColor(btn_U4)
+                        + GetColor(btn_U5)
+                        + GetColor(btn_U6)
+                        + GetColor(btn_U7)
+                        + GetColor(btn_U8)
+                        + GetColor(btn_U9)
+                        + GetColor(btn_R1)
+                        + GetColor(btn_R2)
+                        + GetColor(btn_R3)
+                        + GetColor(btn_R4)
+                        + GetColor(btn_R5)
+                        + GetColor(btn_R6)
+                        + GetColor(btn_R7)
+                        + GetColor(btn_R8)
+                        + GetColor(btn_R9)
+                        + GetColor(btn_F1)
+                        + GetColor(btn_F2)
+                        + GetColor(btn_F3)
+                        + GetColor(btn_F4)
+                        + GetColor(btn_F5)
+                        + GetColor(btn_F6)
+                        + GetColor(btn_F7)
+                        + GetColor(btn_F8)
+                        + GetColor(btn_F9)
+                        + GetColor(btn_D1)
+                        + GetColor(btn_D2)
+                        + GetColor(btn_D3)
+                        + GetColor(btn_D4)
+                        + GetColor(btn_D5)
+                        + GetColor(btn_D6)
+                        + GetColor(btn_D7)
+                        + GetColor(btn_D8)
+                        + GetColor(btn_D9)
+                        + GetColor(btn_L1)
+                        + GetColor(btn_L2)
+                        + GetColor(btn_L3)
+                        + GetColor(btn_L4)
+                        + GetColor(btn_L5)
+                        + GetColor(btn_L6)
+                        + GetColor(btn_L7)
+                        + GetColor(btn_L8)
+                        + GetColor(btn_L9)
+                        + GetColor(btn_B1)
+                        + GetColor(btn_B2)
+                        + GetColor(btn_B3)
+                        + GetColor(btn_B4)
+                        + GetColor(btn_B5)
+                        + GetColor(btn_B6)
+                        + GetColor(btn_B7)
+                        + GetColor(btn_B8)
+                        + GetColor(btn_B9);
+        }
     }
 }
