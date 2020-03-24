ec1d410a5e35363024368f538d3bbf451190d8b2
Tobby Li <tobby.li@festo.com>
2020-03-24 10:38:57 +0800

    can solve cube

--- Festo Rubik‘s Cube Explorer/Form1.Designer.cs
+++ Festo Rubik‘s Cube Explorer/Form1.Designer.cs
@@ -92,6 +92,8 @@
             this.btn_D2 = new System.Windows.Forms.Button();
             this.btn_D1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel_Cube.SuspendLayout();
             this.tableLayoutPanel_U.SuspendLayout();
@@ -1013,12 +1015,32 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(620, 30);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "URULURBDBLUFLRURFDURDRFUDDDLBBFDLFBRBDLBLFUDFRBRFBLFUL";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(791, 527);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 554);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(620, 30);
+            this.textBox2.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 585);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("MetaPlusLF", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -1104,6 +1126,8 @@
         private System.Windows.Forms.Button btn_D2;
         private System.Windows.Forms.Button btn_D1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Festo Rubik‘s Cube Explorer/Form1.cs
+++ Festo Rubik‘s Cube Explorer/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Festo_Rubik_s_Cube_Explorer
 {
@@ -154,6 +155,34 @@ namespace Festo_Rubik_s_Cube_Explorer
                         + GetColor(btn_B7)
                         + GetColor(btn_B8)
                         + GetColor(btn_B9);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string pyexePath = @"kociemba.exe";
+                //python文件所在路径，一般不使用绝对路径，此处仅作为例子，建议转移到debug文件夹下
+
+                Process p = new Process();
+                p.StartInfo.FileName = pyexePath;//需要执行的文件路径
+                p.StartInfo.UseShellExecute = false; //必需
+                p.StartInfo.RedirectStandardOutput = true;//输出参数设定
+                p.StartInfo.RedirectStandardInput = true;//传入参数设定
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.Arguments =  textBox1.Text ;//参数以空格分隔，如果某个参数为空，可以传入””
+                p.Start();
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();//关键，等待外部程序退出后才能往下执行}
+                Console.Write(output);//输出
+                textBox2.Text = output;
+                p.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
         }
     }
 }
